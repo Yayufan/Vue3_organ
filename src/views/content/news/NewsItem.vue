7d779574d0e7aed36e092090d38895350af43935
Yayufan <kamikazey2200@gmail.com>
2024-11-12 12:59:15 +0800

    update

--- src/views/content/news/NewsItem.vue
+++ src/views/content/news/NewsItem.vue
@@ -1,83 +1,105 @@
 <!-- 每個最新消息內容的通用模板 -->
 <template>
-  <section class="news-item-section">
-    <el-card class="news-card-box">
+  <section class="article-item-section">
+    <el-card class="article-item-card-box">
 
-      <el-form :model="newsData" class="news-form" ref="newsForm" :rules="newsRules">
-        <el-form-item label="消息類別" prop="type">
-          <el-input v-model="newsData.type" placeholder="消息類型" />
-        </el-form-item>
-        <el-form-item label="消息標題" prop="title">
-          <el-input v-model="newsData.title" placeholder="消息標題" />
-        </el-form-item>
-        <el-form-item label="公告日期" prop="announcementDate">
-          <el-date-picker v-model="newsData.announcementDate" type="date" placeholder="Pick a Date" format="YYYY-MM-DD"
-            value-format="YYYY-MM-DD" />
-        </el-form-item>
+
+      <el-form :model="article" class="article-form" ref="articleForm" :rules="articleRules" label-position="top">
+
+        <div class="outer-input-box">
+
+          <div class="text-input-box">
+            <el-form-item label="文章類別" prop="type">
+              <el-input v-model="article.type" placeholder="消息類型" />
+            </el-form-item>
+            <el-form-item label="文章標題" prop="title">
+              <el-input v-model="article.title" placeholder="消息標題" />
+            </el-form-item>
+            <el-form-item label="文章描述" prop="description">
+              <el-input type="textarea" v-model="article.description" autocomplete="off" />
+            </el-form-item>
+          </div>
+          <div class="img-box">
+
+            <el-form-item label="縮圖上傳" label-position="top">
+
+              <el-upload class="thumbnail-uploader" :action="envAPI + '/upload/img'" :show-file-list="false"
+                :on-success="handleAvatarSuccess" :before-upload="beforeAvatarUpload">
+                <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+                <el-icon v-else class="avatar-uploader-icon">
+                  <Plus />
+                </el-icon>
+              </el-upload>
+
+            </el-form-item>
+
+          </div>
+        </div>
 
         <div style="text-align: right;">
           <el-button type="info" @click="back">返回</el-button>
-          <el-button type="primary" @click="submitNewsForm(newsForm)">保存</el-button>
+          <el-button type="primary" @click="submitArticleForm(articleForm)">保存</el-button>
         </div>
       </el-form>
 
-      <!-- CKEditor5 編輯器組件   -->
-      <CustomCKEditor :scope="scope" :htmlContent="newsData.content" :updateContent="updateContent">
+      <CustomCKEditor :scope="scope" :htmlContent="article.content" :updateContent="updateContent">
       </CustomCKEditor>
 
     </el-card>
 
-
-    <!-- 頁面顯示 -->
-    <!-- 顯示時要在父標籤上寫class= 'ck-content' -->
-    <div v-html="newsData.content" class="ck-content"></div>
-
   </section>
+
+
 </template>
 
 <script setup lang="ts">
 
 import { ref, reactive } from 'vue'
-import { getArticleApi, updateArticleApi } from '@/api/article'
-import { FormInstance, UploadRawFile } from 'element-plus'
 import CustomCKEditor from '@/components/CustomCKEditor/index.vue'
+import type { FormInstance, FormRules, UploadRawFile, UploadProps } from 'element-plus'
+import { updateArticleApi, getArticleApi } from '@/api/article'
 
 
-//獲取動態路由參數
+//路由參數,這邊注意解構賦值後,會失去響應式
 let { id } = defineProps(['id'])
-
 
 /**  使用CustomCKEditor組件的配置  */
 //數據,父傳子
 const scope = ref("newsScope")
 
-let newsData = reactive({
+const article = reactive<Record<string, any>>({
   articleId: '',
   type: '',
   title: '',
+  description: null,
   content: '',
-  announcementDate: '',
-  tempUploadUrl: <any[]>[]
-})
+  coverThumbnailUrl: ''
+
+});
 
 //方法,子傳父
 const updateContent = (newValue: string) => {
-  newsData.content = newValue;
+  article.content = newValue;
 };
 
+/** 當前組件的配置  */
+const protocol = window.location.protocol; // 获取当前协议 (例如 "http:" 或 "https:")
+const hostname = window.location.hostname; // 获取当前域名 (例如 "www.example.com")
 
-/**  當前組件的配置   */
+const envAPI = import.meta.env.VITE_APP_BASE_API;
+const imageUrl = ref()
+
+//圖片數據
+let imgFile = <UploadRawFile>{}
 
 //獲取路由器
 const router = useRouter()
+const route = useRoute()
+
 //獲取新增最新消息表單DOM
-const newsForm = ref()
-
-//圖片實際數據
-let imgFile = <UploadRawFile>{}
-
+const articleForm = ref()
 //表單校驗規則
-const newsRules = reactive({
+const articleRules = reactive({
   type: [
     {
       required: true,
@@ -91,6 +113,11 @@ const newsRules = reactive({
       message: '標題不能為空',
       trigger: 'blur',
     },
+    {
+      max: 255,
+      message: '內容長度不能超過 255 字符',
+      trigger: 'blur'
+    }
   ],
   announcementDate: [
     {
@@ -98,53 +125,104 @@ const newsRules = reactive({
       message: '公告日期不能為空',
       trigger: 'blur',
     }
+  ],
+  description: [
+    {
+      required: true,
+      message: '描述不能為空',
+      trigger: 'blur',
+    }
   ]
 })
 
 
-//獲取消息
-const getNews = async () => {
+//檔案上傳成功的回調
+const handleAvatarSuccess: UploadProps['onSuccess'] = (
+  response,
+  uploadFile
+) => {
+  console.log(response)
+  console.log(uploadFile)
+
+  imageUrl.value = URL.createObjectURL(uploadFile.raw!)
+
+  //將檔案傳給接收圖片的數據
+  imgFile = uploadFile.raw!
+
+}
+
+//檔案上傳前的回調
+const beforeAvatarUpload: UploadProps['beforeUpload'] = (rawFile) => {
+  if (rawFile.size / 1024 / 1024 > 2) {
+    ElMessage.error('Avatar picture size can not exceed 2MB!')
+    return false
+  }
+  return true
+}
+
+//獲取文章
+const getArticle = async () => {
   let res = await getArticleApi(id)
-  Object.assign(newsData, res.data)
+  Object.assign(article, res.data)
 }
 
 //返回列表
 const back = () => {
-  router.push("/content/news")
+  // 取得當前路徑並按 '/' 分割為陣列
+  const pathSegments = route.path.split('/').filter(Boolean);
+
+  // 確保有上層路徑可回
+  if (pathSegments.length > 1) {
+    // 移除最後一段路徑
+    pathSegments.pop();
+
+    // 組合回去並加上 `/`
+    const parentPath = '/' + pathSegments.join('/');
+
+    // 導航回上層路徑
+    router.push(parentPath);
+
+  } else {
+    //沒有父路由,返回上一個歷史紀錄
+    router.go(-1)
+  }
 }
 
+
 //送出表單
-const submitNewsForm = (form: FormInstance) => {
+const submitArticleForm = (form: FormInstance) => {
   //沒有抓到的這個Dom直接返回
   if (!form) return
   form.validate(async (valid) => {
     if (valid) {
       //獲取儲存在localStorage的臨時上傳檔案URL路徑
       let tempUploadUrlStr = localStorage.getItem(scope.value)
-      //定義變量預設為null
+      //定義變量預設為[]
       let tempUploadUrlList: any[] = [];
 
       //如果上傳檔案URL路徑不為null,則根據逗號分隔
       if (tempUploadUrlStr !== null) {
         tempUploadUrlList = tempUploadUrlStr.split(",");
       }
-      //賦予進newData響應式對象中
-      newsData.tempUploadUrl = tempUploadUrlList
-      console.log(newsData)
+      //賦予進article響應式對象中
+      article.tempUploadUrl = tempUploadUrlList
 
-      let formData = new FormData()
+      let formData = new FormData();
       // 將響應式對象轉換為普通對象，然後轉換為 JSON 字符串
-      const jsonData = JSON.stringify(newsData)
+      const jsonData = JSON.stringify(article)
       formData.append('data', new Blob([jsonData], { type: "application/json" }))
       formData.append('file', imgFile)
+
 
       try {
         //呼叫更新API
         await updateArticleApi(formData)
         //移除臨時上傳檔案URL
         localStorage.removeItem(scope.value)
+        //重製上傳的檔案
+        imgFile = <UploadRawFile>{}
         //保存後返回列表
-        router.push("/content/news")
+        back()
 
         ElMessage.success("儲存成功")
       } catch (err) {
@@ -157,31 +235,72 @@ const submitNewsForm = (form: FormInstance) => {
   })
 }
 
-//頁面掛載時執行
-onMounted(() => {
-  getNews()
-})
 
+onMounted(async () => {
+  await getArticle()
+  imageUrl.value = protocol + '//' + hostname + '/minio' + article.coverThumbnailUrl
+})
 
 </script>
 
+
+
+
 <style scoped lang="scss">
-.news-item-section {
+.article-item-section {
   width: 90%;
   margin: 1% auto;
 
-  .news-card-box {
+  .article-item-card-box {
     min-height: 80vh;
 
-    .news-form {
+    .article-form {
       margin-bottom: 1%;
-      // .el-input {
-      //   --el-input-width: 150px;
-      // }
 
-      // .el-input__inner {
-      //   --el-input-width: 150px;
-      // }
+      .outer-input-box {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        .text-input-box {
+          width: 60%;
+        }
+
+        .img-box {
+          margin-left: -5%
+        }
+      }
+
+      .el-icon.avatar-uploader-icon {
+        font-size: 28px;
+        color: #8c939d;
+        width: 178px;
+        height: 178px;
+        text-align: center;
+      }
+
+      .thumbnail-uploader {
+        .avatar {
+          width: 100%;
+          max-width: 178px;
+          display: block;
+        }
+
+        :deep(.el-upload) {
+          border: 1px dashed var(--el-border-color);
+          border-radius: 6px;
+          cursor: pointer;
+          position: relative;
+          overflow: hidden;
+          transition: var(--el-transition-duration-fast);
+        }
+
+        .el-upload:hover {
+          border-color: var(--el-color-primary);
+        }
+
+      }
+
     }
   }
 }
--- src/views/content/news/index.vue
+++ src/views/content/news/index.vue
@@ -1,116 +1,92 @@
 <!--  -->
 <template>
-
-  <section class="news-section">
-    <el-card class="news-card">
-      <h1>最新消息管理</h1>
-      <ArticleTableNoImg :table="newsList" :getApi="getNews" :addApi="addNews" :deleteApi="deleteNews"
-        :batchDeleteApi="batchDeleteNews" :group="GROUP">
-      </ArticleTableNoImg>
+  <section class="article-section">
+    <el-card class="article-card">
+      <h1>最新消息文章管理</h1>
+      <ArticleTable :table="articleList" :getApi="getArticle" :addApi="addArticle" :deleteApi="deleteArticle"
+        :batchDeleteApi="batchDeleteArticle" :group="GROUP">
+      </ArticleTable>
     </el-card>
-
   </section>
-
 </template>
 
 <script setup lang='ts'>
 
 import { ref, reactive } from 'vue'
-import ArticleTableNoImg from '@/components/ArticleTableNoImg/index.vue'
+import ArticleTable from '@/components/ArticleTable/index.vue'
 import { getAllArticleByGroupByPaginationApi, addArticleApi, deleteArticleApi, batchDeleteArticleApi } from '@/api/article'
+import { transFormPaginationByArticle } from '@/utils/transFormData'
+
 
 //獲取路由
 const route = useRoute()
 //獲取路由器
 const router = useRouter()
 
-//設定article_group 
+//設定article_group , 最重要!!!!!!!
 const GROUP = "news"
 
-/**
- * 用於轉換成子組件需要的分頁對象, 主要是後端的主鍵id 要轉成前端通用的id, 
- * @param response Java mybatis plus IPage對象的返回值
- * @param primaryKeyName 後端實際傳來的主鍵名
- */
-function transFormPagination(response: any, primaryKeyName: any) {
-  //傳入後端響應值,透過解構賦值,提取records(搜索資料) 和 其餘分頁資料
-  const { records, ...paginationData } = response;
-  //使用JS數組的map方法,她會開始遍歷數組,映射並重新組裝
-  const transformedRecords = records.map((record: any) => {
-    //每次拿到一筆資料,透過解構賦值,提取後端資料的newsId 和 其他數據資料; 
-    // [primaryKeyName] 是使用計算屬性的方式,動態決定要解構的屬性,並重新賦予屬姓名,這邊直接取id
-    const { [primaryKeyName]: id, ...rest } = record;
-    //把主鍵名從newsId更換為id , 其餘數據資料不變動,組裝後返回 ; 
-    //這邊注意一定要...rest返回其他key-value, 不然他會包裝成一個叫rest的key-value對象
-    return {
-      ...rest,
-      id
-    };
-  });
-
-  //這邊拿著重新組裝完原始資料的數據 和 其餘分頁資料,進行組裝後返回
-  return {
-    records: transformedRecords,
-    ...paginationData
-  };
-
-}
-
-
-
 //後端獲取Page資料後要傳給子組件的數據
-let newsList = reactive({})
+let articleList = reactive({})
 
-//獲取最新消息
-const getNews = async (page: number, size: number) => {
+//獲取文章
+const getArticle = async (page: number, size: number) => {
   let res = await getAllArticleByGroupByPaginationApi(GROUP, page, size)
-  let transData = transFormPagination(res.data, "articleId")
-  Object.assign(newsList, transData)
-  console.log('這是轉換後的數據', newsList)
+  //固定轉換數據給前端組件使用
+  let transData = transFormPaginationByArticle(res.data, "articleId")
+  Object.assign(articleList, transData)
+  // console.log('這是轉換後的數據', articleList)
 }
 
-//新增最新消息
-const addNews = async (data: any) => {
-  console.log('子組件傳來的data', data)
+//新增文章
+const addArticle = async (data: any) => {
+  // console.log('子組件傳來的data', data)
   let res = await addArticleApi(data)
   const currentPath = route.fullPath
   router.push(currentPath + '/' + res.data)
-  getNews(1, 10)
+  getArticle(1, 10)
 }
 
-//刪除最新消息
-const deleteNews = async (id: number) => {
+//刪除文章
+const deleteArticle = async (id: number) => {
   await deleteArticleApi(id)
-  getNews(1, 10)
+  getArticle(1, 10)
 }
 
-//批量刪除最新消息
-const batchDeleteNews = async (data: any) => {
+//批量文章
+const batchDeleteArticle = async (data: any) => {
   await batchDeleteArticleApi(data)
-  getNews(1, 10)
+  getArticle(1, 10)
 }
 
 //頁面掛載時獲取數據
 onMounted(() => {
-  getNews(1, 10)
+  getArticle(1, 10)
 })
+
+
 
 </script>
 
 <style scoped lang="scss">
-.news-section {
+//這個組件的setion
+.article-section {
   width: 95%;
   margin: 0 auto;
 
-  .news-card {
+
+  //這個組件的card
+  .article-card {
     margin-top: 2%;
     margin-bottom: 2%;
-  }
 
-  h1 {
-    text-align: center;
-    font-size: 2rem;
-    margin: 1% 0;
+    //這個組件的標題
+    h1 {
+      text-align: center;
+      font-size: 2rem;
+      margin: 1% 0;
+    }
+
   }
 }
 </style>
